60ce821ac1006bf352036f414c8596a0d146fbda
Ha-Doan <32703264+Ha-Doan@users.noreply.github.com>
2017-11-15 17:43:48 +0100

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -34,6 +34,6 @@ class ProfilesController < ApplicationController
      end
 
      def profile_params
-       params.require(:profile).permit(:first_name, :last_name)
+       params.require(:profile).permit(:first_name, :last_name, :address, :description)
      end
  end
